93e9e9f876e37844a37867e29f715ff6a1e2a069
kimrichardj@gmail.com <kimrichardj@gmail.com>
2025-12-03 19:27:25 -0800

    UI fix bills and expenses

--- obj/Debug/net10.0-android/Kwiktomes.AssemblyInfo.cs
+++ obj/Debug/net10.0-android/Kwiktomes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kwiktomes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+562a42967ce8333bd19674abcd88afd092b29438")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+75314a1eb46b392af5e1707dab0b4acbe3c2d915")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kwiktomes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kwiktomes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net10.0-windows10.0.19041.0/win-x64/Kwiktomes.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows10.0.19041.0/win-x64/Kwiktomes.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kwiktomes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ea2a3dfa32c909545535d91b22065ce39c3fa8a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+75314a1eb46b392af5e1707dab0b4acbe3c2d915")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kwiktomes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kwiktomes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
